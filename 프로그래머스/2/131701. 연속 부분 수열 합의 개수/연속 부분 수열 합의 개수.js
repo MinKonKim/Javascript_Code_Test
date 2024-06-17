8e540c20012833d8665cbda819bd5123a91a6f8f
김민곤 <57051139+MinKonKim@users.noreply.github.com>
2024-06-17 10:15:07 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 82.89 ms, Memory: 57 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -1,17 +1,13 @@
-function solution(elements) {
-  var answer = 0;
-  const madeNumbers = new Set();
-  const length = elements.length;
-  // 반복할 길이
-  for (let i = 0; i < length; i++) {
-    let sum = 0;
-    const newElements = [...elements, ...elements.slice(0, i)];
-    for (let j = 0; j < newElements.length; j++) {
-      const slicedElements = newElements.slice(j, j + i + 1);
-      const sum = slicedElements.reduce((acc, cur) => acc + cur, 0);
-      madeNumbers.add(sum);
-    }
-  }
 
-  return madeNumbers.size;
+function solution(elements) {
+    const circular = elements.concat(elements);
+    const set = new Set();
+    for (let i = 0; i < elements.length; i++) {
+        let sum = 0;
+        for (let j = 0; j < elements.length; j++) {
+            sum += circular[i + j];
+            set.add(sum);
+        }
+    }
+    return set.size;
 }
