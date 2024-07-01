d470d1fd1a5d89c1b341e230373d3b202ee3a0b8
김민곤 <57051139+MinKonKim@users.noreply.github.com>
2024-07-01 09:26:18 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+
+	int answer = 0;
+	sort(citations.begin(), citations.end()); //오름차순 정렬
+
+	for (int i = 0; i < citations.size(); i++)
+	{
+		int H_idx = citations.size() - i;
+		if (H_idx <= citations[i])return H_idx;
+	}
+
+	return answer;
+}
+
