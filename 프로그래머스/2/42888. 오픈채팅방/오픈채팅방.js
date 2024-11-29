8926668c28cd55b1f55187f311b34cf665480bd1
김민곤 <57051139+MinKonKim@users.noreply.github.com>
2024-11-29 12:10:33 +0900

    [level 2] Title: 오픈채팅방, Time: 430.89 ms, Memory: 92.2 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -4,23 +4,17 @@ function solution(record) {
     record.forEach((rec) => {
         const [state, id, name] = rec.split(" ");
 
-        if (!log[id]) {
-            log[id] = { states: [], name: null };
-        }
-
-        log[id].states.push(state); // 상태 배열에 추가
-
         if (state === "Enter" || state === "Change") {
-            log[id].name = name;
+            log[id] = name;
         }
     });
     
     record.forEach((rec)=>{
         const [state,id]=rec.split(" ");
         if(state==="Enter")
-            answer.push(`${log[id].name}님이 들어왔습니다.`)
+            answer.push(`${log[id]}님이 들어왔습니다.`)
         else if(state==="Leave")
-             answer.push(`${log[id].name}님이 나갔습니다.`)
+             answer.push(`${log[id]}님이 나갔습니다.`)
         
     })
     
